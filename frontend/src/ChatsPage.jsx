d888ed16992744493fb8ae98f0390ac81cd9867f
Navinda Hewawickrama <hewawickraman@gmail.com>
2023-12-09 16:51:46 +0530

    Fourth Commit

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,15 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import {MultiChatSocket,MultiChatWindow, useMultiChatLogic} from 'react-chat-engine-advanced'
+
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic('3697b3bd-20cf-48ff-88e1-389747109de2',
+    props.user.username,
+    props.user.secret
+    );
+    return <div style={{height:'100vh'}}>
+        <MultiChatSocket {...chatProps} />  
+        <MultiChatWindow {...chatProps} style={{height:'100%'}}/>  
+        </div>
 }
 
 export default ChatsPage
